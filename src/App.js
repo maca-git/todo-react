5cee97187a801eadfcf3e04dd22e7b8ece740892
Maxym Tkachuk <m.tkachuk@viseven.com>
2020-11-12 06:47:41 +0200

    update(*): created Alert

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { Alert } from './components/Alert';
 import { Navbar } from './components/Navbar';
+import { AlertState } from './context/alert/AlertState';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
-      <div className="container pt-5">
-        <Alert/>
-        <Switch>
-          <Route path={'/'} exact component={Home}/>
-          <Route path={'/about'} component={About}/>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+      <Navbar/>
+        <div className="container pt-5">
+          <Alert/>
+          <Switch>
+            <Route path={'/'} exact component={Home}/>
+            <Route path={'/about'} component={About}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AlertContext } from '../context/alert/alertContext';
 
 export const Form = () => {
+  const [value, setValue] = useState('')
+  const alert = useContext(AlertContext)
+
+  const submitHandler = e => {
+    e.preventDefault();
+    debugger
+    alert.show(value, 'success')
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
-        <input type="text" className="form-control" placeholder="Enter note title"/>
+        <input 
+          type="text"
+          className="form-control"
+          placeholder="Enter note title"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
       </div>
     </form>
   )
